4e27efda5633da1d9f39ac88632969b116d22882
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-25 17:26:33 -0600

    Add files via upload

---
+++ demofile.py
@@ -0,0 +1,10 @@
+print(" ") # print imprime un espacio
+print("Sanchez Perez briana Sarahi, 1213, 3W")
+print(" ") # print imprime un espacio
+
+f = open("demofile.txt", "r") # abro mi archivo de texo para solo lectura
+print(f.read()) # lee mi archivo
+
+print(" ") # print imprime un espacio
+
+
