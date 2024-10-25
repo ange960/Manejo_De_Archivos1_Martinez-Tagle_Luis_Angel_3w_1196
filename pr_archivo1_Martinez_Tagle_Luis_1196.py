f44e8da1aa9487bfd67ce73b3dec3c6df40207ff
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-25 17:25:12 -0600

    Add files via upload

---
+++ pr_archivo1_Martinez_Tagle_Luis_1196.py
@@ -0,0 +1,27 @@
+print(" ") # print imprime un espacio
+print("Sanchez Perez briana Sarahi, 1213, 3W")
+print(" ") # print imprime un espacio
+
+archivo = open("alumnos.txt","r") # abro mi archivo de texo para solo lectura.
+
+print (archivo.readable()) # se imprime mi archivo
+
+archivo.close() # se cierra el archivo
+
+print(" ") # print imprime un espacio
+
+# "r" - Leer
+# "a" - Anexar 
+# "w" - Escribir 
+# "x" - Crear 
+
+# readable
+# read
+# readlines
+
+# writable
+# write
+# writelines
+
+
+
