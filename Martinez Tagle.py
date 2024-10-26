a4a6b64f758a316c7c34a0f3a0648d8db482d035
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-25 18:03:52 -0600

    Update Martinez Tagle.py

--- Martinez Tagle.py
+++ Martinez Tagle.py
@@ -1,5 +1,5 @@
 print(" ") # print imprime un espacio
-print("Sanchez Perez briana Sarahi, 1213, 3W")
+print("Martinez Tagle Luis Angel 3w 1196")#imprime el nombre del programador y  el numero de control 
 print(" ") # print imprime un espacio
 
 import os # permite eliminar la carpeta
